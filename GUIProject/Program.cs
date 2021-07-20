918ffbf446e8b88cb3ccfe65a260a6f7fd17057f
Karoshee <praslovki@mail.ru>
2021-07-20 19:03:23 +0300

    Add project files.

---
+++ GUIProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GUIProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
